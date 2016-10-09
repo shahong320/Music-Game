4c67c1d160a0f3ea9b3d330d92e703f6fc25bf87
supermax1122 <supermax1122@icloud.com>
2016-10-09 21:09:13 +1300

    dairy fix

--- src/MusicGame/HomeScreen.java
+++ src/MusicGame/HomeScreen.java
@@ -36,108 +36,99 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.Mixer.Info;
 
-
-public class HomeScreen extends JPanel implements ChangeListener{
-	private JPanel menu,volumePanel;
+public class HomeScreen extends JPanel implements ChangeListener {
+	private JPanel menu, volumePanel;
 	private JButton startGame, multiPlay, trophy, gameHistory, exit;
 	private JSlider volume;
 	private JLabel volumeLabel;
-	
-	public HomeScreen(){
+
+	public HomeScreen() {
 		super(new BorderLayout());
-		
-		
-		//Initialize components
+
+		// Initialize components
 		menu = new JPanel(new GridBagLayout());
 		volumePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		GridBagConstraints cons = new GridBagConstraints();
-		startGame=new JButton("Single-Play");
+		startGame = new JButton("Single-Play");
 		getStartGame().setPreferredSize(new Dimension(150, 30));
-		multiPlay=new JButton("Multi-Play");
+		multiPlay = new JButton("Multi-Play");
 		getMultiPlay().setPreferredSize(new Dimension(150, 30));
-		trophy=new JButton("Trophy");
+		trophy = new JButton("Trophy");
 		getTrophy().setPreferredSize(new Dimension(150, 30));
 		gameHistory = new JButton("Game History");
 		gameHistory.setPreferredSize(new Dimension(150, 30));
 		exit = new JButton("Exit");
 		exit.setPreferredSize(new Dimension(150, 30));
 		volumeLabel = new JLabel("Vol.");
-		volume = new JSlider(JSlider.HORIZONTAL,0,100,30);
+		volume = new JSlider(JSlider.HORIZONTAL, 0, 100, 30);
 		volume.setMinorTickSpacing(10);
 		volume.setMajorTickSpacing(50);
 		volume.setPaintTicks(true);
-		volume.setPreferredSize(new Dimension(200,40));
-		
-		
-		
-		//Distribute components
-		cons.insets = new Insets(0,10,10,10);
-		cons.gridx=0;
-		cons.gridy=1;
+		volume.setPreferredSize(new Dimension(200, 40));
+
+		// Distribute components
+		cons.insets = new Insets(0, 10, 10, 10);
+		cons.gridx = 0;
+		cons.gridy = 1;
 		menu.add(getStartGame(), cons);
-		cons.gridx=0;
-		cons.gridy=2;
+		cons.gridx = 0;
+		cons.gridy = 2;
 		menu.add(getMultiPlay(), cons);
-		cons.gridx=0;
-		cons.gridy=3;
+		cons.gridx = 0;
+		cons.gridy = 3;
 		menu.add(getTrophy(), cons);
-		cons.gridx=0;
-		cons.gridy=4;
+		cons.gridx = 0;
+		cons.gridy = 4;
 		menu.add(gameHistory, cons);
-		cons.gridx=0;
-		cons.gridy=5;
+		cons.gridx = 0;
+		cons.gridy = 5;
 		menu.add(exit, cons);
-		
-		
+
 		this.add(menu, BorderLayout.WEST);
 		volumePanel.add(volumeLabel);
 		volumePanel.add(volume);
-		this.add(volumePanel,BorderLayout.SOUTH);
-		
-		
-		
-		//Add slider change listener
+		this.add(volumePanel, BorderLayout.SOUTH);
+
+		// Add slider change listener
 		volume.addChangeListener(this);
-		
-		
+
 	}
-	
-	//Slider changer( Volume change)
+
+	// Slider changer( Volume change)
 	@Override
 	public void stateChanged(ChangeEvent e) {
-		if(e.getSource()==volume){
-			setOutputVolume(volume.getValue()/10);
+		if (e.getSource() == volume) {
+			setOutputVolume(volume.getValue() / 10);
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	public JButton getTrophy() {
+		return trophy;
+	}
 
-		public JButton getTrophy() {
-			return trophy;
-		}
+	public JButton getStartGame() {
+		return startGame;
+	}
 
-		public JButton getStartGame() {
-			return startGame;
-		}
+	public JButton getMultiPlay() {
+		return multiPlay;
+	}
 
-		public JButton getMultiPlay() {
-			return multiPlay;
-		}
+	public JButton getGameHistory() {
+		return gameHistory;
+	}
 
-		public JButton getGameHistory() {
-			return gameHistory;
-		}
+	public JButton getExit() {
+		return exit;
+	}
 
-		public JButton getExit() {
-			return exit;
-		}
-		
-		public static void setOutputVolume(float value)
+	public static void setOutputVolume(float value)
 		{
 		    String command = "set volume " + value;
 		    try
--- src/MusicGame/MainPanel.java
+++ src/MusicGame/MainPanel.java
@@ -32,7 +32,7 @@ public class MainPanel extends JPanel implements ActionListener{
 		history.getBack().addActionListener(this);
 		
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainPanel main=new MainPanel();
